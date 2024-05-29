b2c6345330b275a0e40d639e1d960a61905caabc
unknown <tawhidalam69@gmail.com>
2024-05-29 10:55:47 +0600

    ad and delete data

--- src/Pages/Navigated/BookCard.jsx
+++ src/Pages/Navigated/BookCard.jsx
@@ -1,7 +1,28 @@
+import axios from "axios";
+
+import { toast } from 'react-toastify';
+
 const BookCard = ({book}) => {
-    const {tittle, desc,cover}=book;
+    const {tittle, desc,cover,id}=book;
+    console.log(cover);
+
+    const handleDelete =async (id)=>{
+
+      try{
+          await axios.delete("http://localhost:8000/books/"+id);
+          window.location.reload();
+          toast.success("Deleted successfully !", {
+            position: toast.POSITION.TOP_RIGHT,
+          });
+      }
+      catch(err){
+        console.log(err);
+      }
+
+    }
+
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-100 shadow-xl my-3 mx-3">
       <figure>
         <img
           src={cover}
@@ -15,8 +36,7 @@ const BookCard = ({book}) => {
         </h2>
         <p>{desc}</p>
         <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+        <button onClick={()=>{handleDelete(id)}} className="btn btn-outline btn-warning">Deelete</button>
         </div>
       </div>
     </div>
